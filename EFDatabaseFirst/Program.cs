bffee4f98497f70ca63d52ccb1f4c6a31cb157db
Gaspar <obertigaspar@gmail.com>
2022-02-20 16:12:59 -0300

    se agregó una funcioncita que agrega personas usando EF

--- EFDatabaseFirst/Program.cs
+++ EFDatabaseFirst/Program.cs
@@ -1,11 +1,51 @@
 ﻿using EFDatabaseFirst.Models;
 
-using (var results = new bd_pruebaContext())
-{
-    List<Persona> personas = results.Persona.ToList();
 
-    foreach (var item in personas)
+
+Console.WriteLine($"Listado de Personas");
+getPersonas();
+
+//para agregar un elemento a la tabla lo agrego primero al dbset del contexto y despues guardo los cambios.
+//Persona nuevaPersona = new Persona();
+//nuevaPersona.Nombre = "Jose";
+//nuevaPersona.Apellido = "Perez";
+//nuevaPersona.Dni = 20554696;
+
+//Console.WriteLine($"Insertando Persona...");
+//postPersona(nuevaPersona);
+
+//Console.WriteLine($"Nuevo Listado de Personas");
+//getPersonas();
+
+
+
+
+//-------functions--------
+
+bool postPersona(Persona persona)
+{
+    using (var results = new bd_pruebaContext())
     {
-        Console.WriteLine(item.Nombre);
+        results.Persona.Add(persona);
+        results.SaveChanges();
+    }
+
+    return true;
+}
+
+void getPersonas()
+{
+    using (var results = new bd_pruebaContext())    //contextos con todas las tablas
+    {
+        List<Persona> personas = results.Persona.ToList();  //Persona es un dbset
+        foreach (var item in personas)
+        {
+            Console.WriteLine($"{item.Nombre} {item.Apellido}");
+        }
     }
 }
+
+
+
+
+
